f6cb5e79fb38241dfabe69cd479c538e78e3b26d
budds300 <budds300@gmail.com>
2021-06-05 02:00:46 +0300

    creation of get news function

--- app/request.py
+++ app/request.py
@@ -12,3 +12,31 @@ def configure_request(app):
     base_url_ = app.config['NEWS_API_BASE_URL']
     base_url_articles = app.config['ARTICLES_API_BASE_URL']
 
+def get_news(language):
+    '''
+    Function that gets the json request to our url request
+    '''
+    get_news_url = base_url.format(language,api_key)
+    
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_information = url.read()
+        get_news_response= json.loads(get_news_information)
+        
+        news_sources = None
+        
+        if get_news_response['sources']:
+            news_sources_list= get_news_response['sources']
+            news_sources= process_sources(news_sources_list)
+            
+    return news_sources
+
+def process_sources(news_list):
+      '''
+    Function that processes the news results and transform them to a list of objects
+    
+    Args:
+        news_list: A list of dictionaries that contains movie objects
+        
+    Returns : 
+        news_results : Alist of news objects
+    '''
